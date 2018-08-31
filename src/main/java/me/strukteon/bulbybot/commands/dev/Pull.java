d99a108fba7eb302e1994e201edafd47192ccbe2
blackwolf6549 <37907050+blackwolf6549@users.noreply.github.com>
2018-08-31 09:04:25 +0200

    Added the Pull command.

---
+++ src/main/java/me/strukteon/bulbybot/commands/dev/Pull.java
@@ -0,0 +1,32 @@
+/*
+ * Created by Elias on 31.08.18 08:55
+ *
+ * (c) Elias 2018
+ */
+
+package me.strukteon.bulbybot.commands.dev;
+
+import me.strukteon.bettercommand.CommandEvent;
+import me.strukteon.bettercommand.command.CommandInfo;
+import me.strukteon.bettercommand.command.ExtendedCommand;
+import me.strukteon.bettercommand.syntax.Syntax;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+
+public class Pull implements ExtendedCommand {
+
+    @Override
+    public void onExecute(CommandEvent event, Syntax syntax, User author, MessageChannel channel) throws Exception {
+
+        event.getTextChannel().sendMessage("Pulling started!").queue();
+        Runtime.getRuntime().exec("cmd bash /home/bulby.sh");
+
+    }
+
+    @Override
+    public CommandInfo getCommandInfo() {
+        return new CommandInfo("pull")
+                .setHelp("Pulls the newest version from github!");
+    }
+
+}
