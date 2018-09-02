08d17ad3b314b3031242a324e2cd52db0985b0a6
Strukteon <mail@strukteon.me>
2018-09-02 16:03:57 +0200

    changed some things with the jdoodle api

--- src/main/java/me/strukteon/bulbybot/commands/general/Execute.java
+++ src/main/java/me/strukteon/bulbybot/commands/general/Execute.java
@@ -60,9 +60,8 @@ public class Execute implements ExtendedCommand {
                                 .put("versionIndex", versionCodes[codeIndex]).toString());
                 ResponseBody responseBody = client.newCall(new Request.Builder().post(requestBody).url(API_URL).build()).execute().body();
                 JSONObject res = new JSONObject(responseBody.string());
-                System.out.println(res.toString());
                 if (res.getInt("statusCode") == 200)
-                    response.addField("Additional info:", String.format("CPU time: **%ss**\nMemory usage: **%skB**", res.getDouble("cpuTime"), res.getInt("memory")/1024), false)
+                    response.addField("Additional info:", String.format("CPU time: **%ss**\nMemory usage: **%skB**", res.get("cpuTime"), (res.get("memory") == null ? -1024 : res.getInt("memory"))/1024), false)
                             .addField("Output:", "```" + res.getString("output") + "```", false);
                 else
                     response.addField("Status code:", res.getInt("statusCode") + "", false)
