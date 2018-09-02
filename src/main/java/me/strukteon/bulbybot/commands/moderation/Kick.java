5f58f6cf0db513b252e9a9606d0fbf1dd186ff01
blackwolf6549 <37907050+blackwolf6549@users.noreply.github.com>
2018-09-02 10:36:23 +0200

    Small fix

--- src/main/java/me/strukteon/bulbybot/commands/moderation/Kick.java
+++ src/main/java/me/strukteon/bulbybot/commands/moderation/Kick.java
@@ -1,5 +1,5 @@
 /*
- * Created by Elias on 01.09.18 13:32
+ * Created by Elias on 31.08.18 18:48
  *
  * (c) Elias 2018
  */
@@ -8,58 +8,54 @@ package me.strukteon.bulbybot.commands.moderation;
 
 import me.strukteon.bettercommand.CommandEvent;
 import me.strukteon.bettercommand.command.CommandInfo;
-import me.strukteon.bettercommand.command.ExtendedCommand;
+import me.strukteon.bettercommand.command.ExtendedGuildCommand;
 import me.strukteon.bettercommand.command.PermissionManager;
 import me.strukteon.bettercommand.syntax.Syntax;
 import me.strukteon.bettercommand.syntax.SyntaxBuilder;
 import me.strukteon.bettercommand.syntax.SyntaxElementType;
 import me.strukteon.bulbybot.utils.ChatTools;
 import me.strukteon.bulbybot.utils.PermissionsCheck;
-import me.strukteon.bulbybot.utils.Static;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.Guild;
-import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.*;
 
 import java.util.List;
 
-public class Kick implements ExtendedCommand {
+public class Ban implements ExtendedGuildCommand {
 
     @Override
-    public void onExecute(CommandEvent event, Syntax syntax, User author, MessageChannel channel) throws Exception {
+    public void onExecute(CommandEvent event, Syntax syntax, Member author, TextChannel channel) throws Exception {
 
-        if (!(PermissionsCheck.hasPermission(event, event.getMember(), Permission.KICK_MEMBERS)))
+        if (!(PermissionsCheck.hasPermission(event, event.getMember(), Permission.BAN_MEMBERS)))
             return;
 
         List<Member> mentionedMembers = syntax.getAsListMember("users");
         Guild guild = event.getGuild();
         int selPosition = guild.getSelfMember().getRoles().get(0).getPosition();
         int authorPosition = event.getMember().getRoles().get(0).getPosition();
-        StringBuilder kicked = new StringBuilder();
-        StringBuilder notKicked = new StringBuilder();
+        StringBuilder banned = new StringBuilder();
+        StringBuilder notBanned = new StringBuilder();
 
         mentionedMembers.forEach(member -> {
 
             if (member.getRoles().get(0).getPosition() >= selPosition || member.getRoles().get(0).getPosition() >= authorPosition || member.isOwner()) {
-                notKicked.append((notKicked.length() > 0 ? ", " : "") + member.getAsMention());
+                notBanned.append((notBanned.length() > 0 ? ", " : "") + member.getAsMention());
             }
             else {
-                guild.getController().kick(member).reason("Command executed by " + author.getName()).queue();
-                kicked.append((kicked.length() > 0 ? ", " : "") + member.getAsMention());
+                guild.getController().ban(member, 0).reason("Command executed by " + author.getEffectiveName()).queue();
+                banned.append((banned.length() > 0 ? ", " : "") + member.getAsMention());
             }
 
         });
 
-        
+
 
         EmbedBuilder eb = ChatTools.INFO(author)
-            .setTitle("Kick-List");
-        if (notKicked.length() > 0)
-            eb.setDescription("**Not kicked members (you/I have no permissions)**\n"+notKicked);
-        if (kicked.length() > 0)
-            eb.appendDescription((notKicked.length() > 0 ? "\n\n" : "") + "**Successfully kicked members**\n"+kicked);
+                .setTitle("Ban-List");
+        if (notBanned.length() > 0)
+            eb.setDescription("**Not banned members (you/I have no permissions)**\n"+notBanned);
+        if (banned.length() > 0)
+            eb.appendDescription((notBanned.length() > 0 ? "\n\n" : "") + "**Successfully banned members**\n"+banned);
 
         channel.sendMessage(eb.build()).queue();
 
@@ -68,18 +64,17 @@ public class Kick implements ExtendedCommand {
     @Override
     public PermissionManager getPermissionManager() {
         return new PermissionManager()
-                .addRequiredUserPerms(Permission.KICK_MEMBERS)
-                .addRequiredBotPerms(Permission.KICK_MEMBERS);
+            .addRequiredUserPerms(Permission.BAN_MEMBERS)
+            .addRequiredBotPerms(Permission.BAN_MEMBERS);
     }
 
     @Override
     public CommandInfo getCommandInfo() {
-        return new CommandInfo("kick")
-            .setHelp("Kicks all mentioned users!")
+        return new CommandInfo("ban")
+            .setHelp("Bans all mentioned users!")
             .setSyntaxBuilder(
                 new SyntaxBuilder()
                     .addElement("users", SyntaxElementType.MEMBER, true)
             );
     }
-
 }
