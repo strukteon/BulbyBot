e43f502da63a9e57d0c39e442c66009c53f73b5a
blackwolf6549 <37907050+blackwolf6549@users.noreply.github.com>
2018-09-03 10:28:39 +0200

    Added Autochannels!

---
+++ src/main/java/me/strukteon/bulbybot/commands/moderation/Autochannel.java
@@ -0,0 +1,46 @@
+/*
+ * Created by Elias on 03.09.18 10:17
+ *
+ * (c) Elias 2018
+ */
+
+package me.strukteon.bulbybot.commands.moderation;
+
+import me.strukteon.bettercommand.CommandEvent;
+import me.strukteon.bettercommand.command.CommandInfo;
+import me.strukteon.bettercommand.command.ExtendedGuildCommand;
+import me.strukteon.bettercommand.command.PermissionManager;
+import me.strukteon.bettercommand.syntax.Syntax;
+import me.strukteon.bulbybot.utils.ChatTools;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+public class Autochannel implements ExtendedGuildCommand {
+
+    @Override
+    public void onExecute(CommandEvent event, Syntax syntax, Member author, TextChannel channel) throws Exception {
+
+        EmbedBuilder eb = ChatTools.INFO(author)
+                .setDescription("Put this emote \\⏬ anywhere in a voicechannel-name and join the channel to create a Autochannel!");
+
+        channel.sendMessage(eb.build()).queue();
+
+    }
+
+    @Override
+    public PermissionManager getPermissionManager() {
+        return new PermissionManager()
+                .addRequiredBotPerms(Permission.MANAGE_CHANNEL)
+                .addRequiredUserPerms(Permission.MANAGE_CHANNEL);
+    }
+
+    @Override
+    public CommandInfo getCommandInfo() {
+        return new CommandInfo("autochannel")
+                .setAliases("autochannels")
+                .setHelp("Shows you how to create Autochannels.");
+    }
+
+}
