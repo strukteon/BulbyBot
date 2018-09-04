bd2b50a57217925e3b2cdd1afaf8fc10bd11e382
Strukteon <mail@strukteon.me>
2018-09-04 14:23:29 +0200

    You can now get money by writing in the chat

--- src/main/java/me/strukteon/bulbybot/listeners/XPListener.java
+++ src/main/java/me/strukteon/bulbybot/listeners/XPListener.java
@@ -7,13 +7,25 @@ package me.strukteon.bulbybot.listeners;
 
 import me.strukteon.bulbybot.core.sql.UserSQL;
 import me.strukteon.bulbybot.utils.LevelSystem;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class XPListener extends ListenerAdapter {
+    private static Map<String, Long> cooldown = new HashMap<>();
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (UserSQL.existsFromUser(event.getAuthor()))
-            UserSQL.fromUser(event.getAuthor()).addXp(LevelSystem.getXp(event.getMessage().getContentRaw().length()));
+        User author = event.getAuthor();
+        if (UserSQL.existsFromUser(author)) {
+            UserSQL userSQL = UserSQL.fromUser(author);
+            userSQL.addXp(LevelSystem.getXp(event.getMessage().getContentRaw().length()));
+            if (!cooldown.containsKey(author.getId()) || cooldown.get(author.getId()) < System.currentTimeMillis() - 10000) {
+                cooldown.replace(author.getId(), System.currentTimeMillis());
+                userSQL.addMoney(1);
+            }
+        }
     }
 }
